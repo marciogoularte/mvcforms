36c989c7f9f6883372eee5f1763266363a19368c
tsavage <tsavage@localhost>
2010-02-02 17:27:41 +1100

    - Removed unwanted using message - converted tabs to spaces

--- Templates/MvcForms.Form/Form.cs
+++ Templates/MvcForms.Form/Form.cs
@@ -1,24 +1,23 @@
 namespace $rootnamespace$
 {
-	using System.Collections.Specialized;
-	using System.Web;
+    using System.Collections.Specialized;
 
     using JL.Web.Forms;
     using Fields = JL.Web.Forms.Fields;
     using Widgets = JL.Web.Forms.Widgets;
     
     
-	public class $safeitemrootname$ : Form
-	{
-		// Example:
-		//
-		// public static IField MyField = new Fields.StringField 
-		// { 
-		//     MaxLength = 100,
-		// };
-		
-		#region .ctors
-		
+    public class $safeitemrootname$ : Form
+    {
+        // Example:
+        //
+        // public static IField MyField = new Fields.StringField 
+        // { 
+        //     MaxLength = 100,
+        // };
+        
+        #region .ctors
+        
         /// <summary>
         /// Construct form object.
         /// </summary>
@@ -28,15 +27,17 @@ namespace $rootnamespace$
         /// Construct form object.
         /// </summary>
         /// <param name="data">Data to bind form to.</param>
-        public $safeitemrootname$(NameValueCollection data) : base(data, null) { }
+        public $safeitemrootname$(NameValueCollection data) 
+            : base(data, null) { }
         
         /// <summary>
         /// Construct form object.
         /// </summary>
         /// <param name="data">Data to bind form to.</param>
         /// <param name="files">File data to bind form to.</param>
-        public $safeitemrootname$(NameValueCollection data, HttpFileCollectionBase files) : base(data, files) { }
-		
-		#endregion
-	}
+        public $safeitemrootname$(NameValueCollection data, System.Web.HttpFileCollectionBase files) 
+            : base(data, files) { }
+        
+        #endregion
+    }
 }
--- Templates/MvcForms.FormGroup/FormGroup.cs
+++ Templates/MvcForms.FormGroup/FormGroup.cs
@@ -1,22 +1,21 @@
 namespace $rootnamespace$
 {
-	using System.Collections.Specialized;
-	using System.Web;
+    using System.Collections.Specialized;
 
     using JL.Web.Forms;
     
     
-	public class $safeitemrootname$ : FormGroup
-	{
-		// Example:
-		//
-		// public IForm Example = new ExampleForm
-		// {
-		//     Label = "Example form label",
-		// };
-		
-		#region .ctors
-		
+    public class $safeitemrootname$ : FormGroup
+    {
+        // Example:
+        //
+        // public IForm Example = new ExampleForm
+        // {
+        //     Label = "Example form label",
+        // };
+        
+        #region .ctors
+        
         /// <summary>
         /// Construct form group object.
         /// </summary>
@@ -26,15 +25,17 @@ namespace $rootnamespace$
         /// Construct form group object.
         /// </summary>
         /// <param name="data">Data to bind form to.</param>
-        public $safeitemrootname$(NameValueCollection data) : base(data, null) { }
+        public $safeitemrootname$(NameValueCollection data) 
+            : base(data, null) { }
         
         /// <summary>
         /// Construct form group object.
         /// </summary>
         /// <param name="data">Data to bind form to.</param>
         /// <param name="files">File data to bind form to.</param>
-        public $safeitemrootname$(NameValueCollection data, HttpFileCollectionBase files) : base(data, files) { }
-		
-		#endregion
-	}
+        public $safeitemrootname$(NameValueCollection data, System.Web.HttpFileCollectionBase files) 
+            : base(data, files) { }
+        
+        #endregion
+    }
 }
